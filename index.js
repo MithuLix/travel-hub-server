4289c338cff0062b4041dd6019eb590f6c223147
mithulix <mohaiminul.devs@gmail.com>
2022-07-05 23:47:09 -0400

    gsrx

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const port = 5000;
 const objectId = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
 const { ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sckbv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tbdbuji.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//db working/not
-app.get("/",(req, res) => { res.send("TravelHub Server-Database connected!");})
+// db working/not
+app.get("/", (req, res) => { res.send("TravelHub Server-Database connected!"); })
 
 //DB connection between client-server
 client.connect(err => {
-//Database
+    //Database
     const Destinations = client.db("travelHub").collection("destinations");
     const Orders = client.db("travelHub").collection("orders");
     const Reviews = client.db("travelHub").collection("reviews");
@@ -27,79 +27,78 @@ client.connect(err => {
 
 
 
-//HOME
+    //HOME
     //get destinations
     app.get('/getDestinations', (req, res) => {
         Destinations.find({})
-        .toArray((err, documents) => {res.send(documents)})
+            .toArray((err, documents) => { res.send(documents) })
     })
-    app.get('/getDestinations/:id', (req, res)=>{
-        Destinations.find({_id : ObjectId(req.params.id)})
-        .toArray((err, documents)=>{ res.send(documents[0])})
+    app.get('/getDestinations/:id', (req, res) => {
+        Destinations.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => { res.send(documents[0]) })
     })
     // -------------------------------
 
+5
 
-
-//BookingPanel
+    //BookingPanel
     //new Book
     app.post('/newBooking', (req, res) => {
         Orders.insertOne(req.body)
-        .then(result => {res.send(result.insertedCount > 0)})
+            .then(result => { res.send(result.insertedCount > 0) })
     })
     //show BookingList
     app.get('/getBookingList', (req, res) => {
-        Orders.find({email : req.query.email})
-        .toArray((err, documents) => {res.send(documents)})
+        Orders.find({ email: req.query.email })
+            .toArray((err, documents) => { res.send(documents) })
     })
     //add a review
     app.post('/addReview', (req, res) => {
         Reviews.insertOne(req.body)
-        .then(result => {res.send(result.insertedCount > 0)})
+            .then(result => { res.send(result.insertedCount > 0) })
     })
     //get reviews
     app.get('/getReviews', (req, res) => {
         Reviews.find({})
-        .toArray((err, documents) => {res.send(documents)})
+            .toArray((err, documents) => { res.send(documents) })
     })
     // -----------------------------
+    
 
-
-
-// AdminPanel
+    // AdminPanel
     //All Order List
     app.get('/allOrderList', (req, res) => {
         Orders.find({})
-        .toArray((err, documents) => {res.send(documents)})
+            .toArray((err, documents) => { res.send(documents) })
     });
     //update Orders
     app.patch('/updateOrder/:id', (req, res) => {
-        Destinations.updateOne({_id: ObjectId(req.params.id)}, 
-                        {$set: {status: req.body.status}})
-        .then(result => {res.send(result.modifiedCount > 0)})
+        Destinations.updateOne({ _id: ObjectId(req.params.id) },
+            { $set: { status: req.body.status } })
+            .then(result => { res.send(result.modifiedCount > 0) })
     })
     //create destinations
     app.post('/addDestinations', (req, res) => {
         Destinations.insertOne(req.body)
-        .then(result => {res.send(result.insertedCount > 0)})
+            .then(result => { res.send(result.insertedCount > 0) })
     });
     //deleteDestination
     app.delete('/deleteDestination/:id', (req, res) => {
-        Destinations.deleteOne({_id :objectId(req.params.id)})
-        .then(result => {res.send(result.deletedCount > 0)})
+        Destinations.deleteOne({ _id: objectId(req.params.id) })
+            .then(result => { res.send(result.deletedCount > 0) })
     })
     //makeAdmin
     app.post('/makeAdmin', (req, res) => {
         Admin.insertOne(req.body)
-        .then(result => {res.send(result.insertedCount > 0)})
+            .then(result => { res.send(result.insertedCount > 0) })
     });
     //getAdmin
     app.get('/getAdmin', (req, res) => {
         Admin.find({})
-        .toArray((err, documents) => {res.send(documents)})
+            .toArray((err, documents) => { res.send(documents) })
     });
     // -----------------------------
 
-  });
+});
 
 app.listen(process.env.PORT || port)
